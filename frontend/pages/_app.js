a5570e548d0b1bca193860b55358e51c3e165f07
pejantantangguh <suherman.ng@gmail.com>
2018-12-19 20:18:20 +1000

    First Commit for base Prettier and eslint config

---
+++ frontend/pages/_app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import App, { Container } from "next/app";
+import Head from "next/head";
+
+class myApp extends App {
+  static async getInitialProps({ component, router, ctx }) {
+    let pageProps = {};
+  }
+}
