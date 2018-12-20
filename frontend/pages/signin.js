62b5a7e8038b76486396a8560025a009430d0b83
pejantantangguh <suherman.ng@gmail.com>
2018-12-21 09:31:36 +1000

    Adding Sign in and Sign Out

---
+++ frontend/pages/signin.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default () => {
+  return (
+    <Layout>
+      <h2>Sign In</h2>
+    </Layout>
+  );
+};
---
+++ frontend/pages/signup.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default () => {
+  return (
+    <Layout>
+      <h2>Sign Up</h2>
+    </Layout>
+  );
+};
